7ea5e5588a96c358494e194204043203174ca44d
Kevin Chi <kevinchi118@gmail.com>
2018-06-25 02:10:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,8 +17,7 @@ class Scraper
   end
 
   def get_page(page)
-      Nokogiri::HTML(open("#{page}"))
-
+      Nokogiri::HTML(open("https://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
 end
